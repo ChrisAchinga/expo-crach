d7156896fda60886f47c00ef6b2cd4609c8cc4d8
C A <achinga.chris@gmail.com>
2024-07-14 23:41:26 +0300

    bundle as apk ...

---
+++ app/src/main/java/app/web/dadjokes_6996/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.web.dadjokes_6996.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
